dbaa219eb3d445b6b2b57fb7054c46c613178b4d
Andi Cirjaliu <Andi.Cirjaliu@ro.ibm.com>
2020-10-23 18:20:36 +0300

    Created prod version of Docker

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders hello message', () => {
+  const { getByText } = render(<App />);
+  const helloElement = getByText(/Hi there/i);
+  expect(helloElement).toBeInTheDocument();
+});
